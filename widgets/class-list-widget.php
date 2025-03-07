931296c633908429cf4437b9bbe8df0cbca541a7
Victor <victor3790@gmail.com>
2025-03-06 19:35:16 -0600

    ENH: list rendered.

--- widgets/class-list-widget.php
+++ widgets/class-list-widget.php
@@ -48,12 +48,115 @@ class List_Widget extends \Elementor\Widget_Base {
 	}
 
 	/**
+	 * Register the custom controls here
+	 */
+	protected function register_controls(): void {
+
+		// Content Tab Start.
+
+		$this->start_controls_section(
+			'section_repeater',
+			array(
+				'label' => esc_html__( 'Select a repeater field', 'show-acf-repeater-as-a-list' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+			)
+		);
+
+		$repeater_list = array();
+
+		$current_post = get_queried_object();
+		$post_id      = $current_post ? $current_post->ID : null;
+
+		if ( ! $post_id ) {
+			return;
+		}
+
+		$field_groups = acf_get_field_groups( array( 'post_id' => $post_id ) );
+
+		foreach ( $field_groups as $group ) {
+			$field_group_id = $group['ID'];
+			$fields         = acf_get_fields( $field_group_id );
+
+			foreach ( $fields as $field ) {
+				if ( 'repeater' === $field['type'] ) {
+					$repeater_list[ $field['key'] ] = $field['label'];
+				}
+			}
+		}
+
+		$this->add_control(
+			'repeater_key',
+			array(
+				'label'   => esc_html__( 'Repeater', 'show-acf-repeater-as-a-list' ),
+				'type'    => \Elementor\Controls_Manager::SELECT,
+				'options' => $repeater_list,
+			)
+		);
+
+		$this->end_controls_section();
+
+		// Content Tab End.
+
+		// Style Tab Start.
+
+		$this->start_controls_section(
+			'section_list_style',
+			array(
+				'label' => esc_html__( 'List', 'show-acf-repeater-as-a-list' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+			)
+		);
+
+		$this->add_control(
+			'text_color',
+			array(
+				'label'     => esc_html__( 'Text Color', 'show-acf-repeater-as-a-list' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => array(
+					'{{WRAPPER}} .repeater-list' => 'color: {{VALUE}};',
+				),
+			)
+		);
+
+		$this->add_control(
+			'font_family',
+			array(
+				'label'     => esc_html__( 'Font Family', 'show-acf-repeater-as-a-list' ),
+				'type'      => \Elementor\Controls_Manager::FONT,
+				'default'   => "'Open Sans', sans-serif",
+				'selectors' => array(
+					'{{WRAPPER}} .repeater-list' => 'font-family: {{VALUE}}',
+				),
+			)
+		);
+
+		$this->end_controls_section();
+
+		// Style Tab End.
+	}
+
+	/**
 	 * The render function for the widget.
 	 */
 	protected function render(): void {
-		?>
-		<p> Hello World </p>
-		<?php
+		$current_post = get_queried_object();
+		$post_id      = $current_post ? $current_post->ID : null;
+
+		$settings = $this->get_settings_for_display();
+
+		if ( empty( $settings['repeater_key'] ) ) {
+			return;
+		}
+
+		$repeater_rows = get_field( $settings['repeater_key'], $post_id );
+
+		if ( $repeater_rows ) {
+			echo '<ul class="repeater-list">';
+			foreach ( $repeater_rows as $row ) {
+				echo '<li>' . esc_html( reset( $row ) ) . '</li>';
+			}
+			echo '</ul>';
+		}
 	}
 
 	/**
@@ -61,7 +164,14 @@ class List_Widget extends \Elementor\Widget_Base {
 	 */
 	protected function content_template(): void {
 		?>
-		<p> Hello World </p>
+		<#
+		if ( '' === settings.repeater_key ) {
+			return;
+		}
+		#>
+		<p>
+			{{ settings.repeater_key }}
+		</p>
 		<?php
 	}
 }
